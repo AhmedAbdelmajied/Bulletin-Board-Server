3e7e8abf4c45f0ab1f692294a109c8fd86b68bb8
JihadIsmail <nooneknow2050@gmail.com>
2015-02-23 11:20:57 +0200

    Writer class has been modified.

--- src/Writer.java
+++ src/Writer.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 import comunication.SenderReceiver;
 import database.SharedObject;
 
@@ -5,6 +7,11 @@ public class Writer extends Thread {
 
 	private SenderReceiver senderReceiver;
 	private SharedObject sharedObject;
+	private String foundCmnd = "200 OK" ;
+	private String errNotFoundCmnd = "404 Not Found" ;
+	private String crtdCmnd = "201 Created";
+	private String errNotAccptCmnd = "406 Not Acceptable";
+	
 
 	private boolean stoped;
 
@@ -17,6 +24,32 @@ public class Writer extends Thread {
 
 	public void run() {
 		while (!stoped) {
+			String cmnd= new String (senderReceiver.receive());
+			
+			String [] cmnd_arr = cmnd.split("\\s+") ;  // split on any special character
+			
+			if(cmnd_arr[0].equals("write")){
+				cmnd = foundCmnd+ sharedObject.getData();
+				
+				try {
+					
+					senderReceiver.send(cmnd.getBytes()) ;
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else{
+				try {
+					
+					senderReceiver.send(errNotFoundCmnd.getBytes()) ;
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			
 
 		}
 	}
